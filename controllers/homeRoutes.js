cc5565286e72e0b78c5d6ef753499467dc1fa773
PeterJanasz <petertjanasz@gmail.com>
2023-09-21 21:03:31 -0700

    created GET login in homeRoutes and made some minor changes to login and main handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,6 +15,16 @@ router.get('/', async (req, res) => {
     } catch(err) {
         res.status(500).json(err);
     }
-})
+});
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/homepage');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 module.exports = router;
