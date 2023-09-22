04b5dc0a0b562cae2f53cd1841297f9c26281030
PeterJanasz <petertjanasz@gmail.com>
2023-09-21 18:29:37 -0700

    created models, seed.js, env localhost

---
+++ seeds/seed.js
@@ -0,0 +1,34 @@
+const sequelize = require('../config/connection');
+const { User, Seat, Movie } = require('../models');
+
+const userData = require('./userData.json');
+const seatData = require('./seatData.json');
+const movieData = require('./movieData.json');
+
+const seedDatabase = async () => {
+  try {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+
+    const users = await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+
+    for (const seat of seatData) {
+      await Seat.create({
+        ...seat, 
+      });
+    }
+
+    await Movie.bulkCreate(movieData); 
+    console.log('\n----- MOVIE SEEDED -----\n');
+
+    process.exit(0);
+  } catch (error) {
+    console.error('Error seeding database:', error);
+    process.exit(1);
+  }
+};
+
+seedDatabase();
