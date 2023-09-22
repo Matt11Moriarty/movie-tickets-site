ee5f77d76826980b484dbaa4ec10f67a3e16aa1c
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-09-21 18:59:31 -0700

    added routets to homeRoutes and updated the index.js's

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,8 @@
-//
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const seatRoutes = require('./seatRoutes');
+
+router.use('/users', userRoutes);
+router.use('/seats', seatRoutes);
+
+module.exports = router;
